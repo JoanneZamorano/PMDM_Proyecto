5495165f55e615ed6ab2be8a9c8f348b6d52d994
JoanneZamorano <108297574+JoanneZamorano@users.noreply.github.com>
2026-04-28 21:42:15 +0200

    apartado video terminado

---
+++ app/src/main/java/com/example/pmdm_proyecto/ListActivity.kt
@@ -0,0 +1,32 @@
+package com.example.pmdm_proyecto
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class ListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView((R.layout.activity_list))
+
+        val recycler = findViewById<RecyclerView>(R.id.recyclerVideos)
+        recycler.layoutManager = LinearLayoutManager(this)
+
+        var lista = listOf(
+            Video ("Introducción a D&D", R.drawable.bookrole, R.raw.videoplayback),
+            Video ("Catacumbas", R.drawable.bookrole, R.raw.catacumbas),
+            Video ("Bosque", R.drawable.bookrole, R.raw.forestvideo),
+        )
+
+
+        val adapter = VideoAdapter(lista){video ->
+            val intent = Intent(this, VideoActivity::class.java)
+            intent.putExtra("video", video.video)
+            startActivity(intent)
+        }
+
+        recycler.adapter = adapter
+
+    }
+}
---
+++ app/src/main/java/com/example/pmdm_proyecto/Video.kt
@@ -0,0 +1,7 @@
+package com.example.pmdm_proyecto
+
+class Video (
+    val titulo: String,
+    val imagen: Int,
+    val video: Int
+)
